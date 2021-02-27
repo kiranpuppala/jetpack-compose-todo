1cfce0c372405938504f362c6b38fa4f3dbcdedb
kiranpuppala <kiranpuppala.96@gmail.com>
2021-02-27 10:31:29 +0530

    Add README.md

--- app/src/main/java/com/kiranpuppala/jetpacktodo/MainActivity.kt
+++ app/src/main/java/com/kiranpuppala/jetpacktodo/MainActivity.kt
@@ -2,7 +2,7 @@ package com.kiranpuppala.jetpacktodo
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.ui.platform.setContent
+import androidx.activity.compose.setContent
 import com.kiranpuppala.jetpacktodo.ui.theme.JetpackTodoTheme
 
 class MainActivity : AppCompatActivity() {
